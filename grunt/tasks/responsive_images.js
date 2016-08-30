cff1270e61a12ae925840559c579deb21a7a0928
Adam Petrie <adam.petrie@wishabi.com>
2016-08-30 14:21:59 -0400

    Add responsive images to grunt

---
+++ grunt/tasks/responsive_images.js
@@ -0,0 +1,19 @@
+module.exports = {
+  default: {
+    options: {
+      engine: 'gm',
+      sizes: [{ 
+        name: 'thumb',
+        width: 100,
+        height: 100,
+        aspectRatio: false
+      }]
+    },
+    files: [{
+      expand: true,
+      src: ['**/*.{jpg,jpeg}', '!carousel/*.{jpg,jpeg}'],
+      cwd: '<%= paths.siteAssets %>/images',
+      dest: '<%= paths.siteAssets %>/images'
+    }]
+  }
+}
